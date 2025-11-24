018350fc729663509a86996322e4454764fa1abb
Jässin Aouani <aouanijassin@gmail.com>
2025-11-24 09:27:47 +0100

    aufgabe 5 :check:

--- main.py
+++ main.py
@@ -165,36 +165,43 @@ class AVLBaum(Baum):
 
 
 # Umsetzung
-bst = Baum(Node(5))
-bst.insert(3)
-bst.insert(7)
-bst.insert(1)
-bst.insert(9)
-bst.insert(10)
-bst.insert(2)
-bst.insert(12)
-bst.insert(0)
-bst.insert(11)
+print("-------------")
 
-print(bst)
 
-print(bst.get_height())
+# Aufgabe 4 - Datensatz mit 10 zufälligen Schlüsseln
+# (erzeugt via `[randint(0, 100) for _ in range(10)]`)
+print("Aufgabe 4:")
 
-print("-----")
+numbers: list[int] = [69, 76, 68, 16, 51, 84, 7, 18, 49, 53]
+test_bst = Baum(Node(numbers[0]))
+for value in numbers[1:]:
+    test_bst.insert(value)
+print(test_bst)
 
-avl_bst = AVLBaum(Node(5))
-avl_bst.insert(3)
-avl_bst.insert(7)
-avl_bst.insert(1)
-avl_bst.insert(9)
-avl_bst.insert(93)
-avl_bst.insert(2)
-avl_bst.insert(99)
-avl_bst.insert(0)
-avl_bst.insert(91)
+print("-------------")
 
-print(avl_bst)
+print("Erstelle BSTs mit den Einträgen aus dem Dataset...")
 
-print(avl_bst.get_height())
+all_bsts: list[Baum] = []
 
-# print(DATASET)
+for entry in DATASET:
+    bst = Baum(Node(entry[0]))
+    for value in entry[1:]:
+        bst.insert(value)
+    all_bsts.append(bst)
+
+print("Erstelle AVL-BSTs mit den Einträgen aus dem Dataset...")
+
+all_avl_bsts: list[AVLBaum] = []
+
+for entry in DATASET:
+    avl_bst = AVLBaum(Node(entry[0]))
+    for value in entry[1:]:
+        avl_bst.insert(value)
+    all_avl_bsts.append(avl_bst)
+
+print("fertig...\n-----------")
+print("Durchschnittliche Höhe der BSTs:")
+print(sum([tree.get_height() for tree in all_bsts]) / len(all_bsts))
+print("Vergleich: Durchschnittliche Höhe der AVL BSTs:")
+print(sum([tree.get_height() for tree in all_avl_bsts]) / len(all_avl_bsts))
