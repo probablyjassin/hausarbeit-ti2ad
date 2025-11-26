3006d4954a9184e179c57b577c55add1753687c9
Jässin Aouani <aouanijassin@gmail.com>
2025-11-26 13:29:11 +0100

    fixed the formatting of the output of main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from baum import Node, Baum, AVLBaum
 from daten import DATASET
 
-# Umsetzung
-print("-------------")
-
 # Aufgabe 4 - Datensatz mit 10 zufälligen Schlüsseln
 # (erzeugt via `[randint(0, 100) for _ in range(10)]`)
+
 print("Aufgabe 4:")
 
 numbers: list[int] = [69, 76, 68, 16, 51, 84, 7, 18, 49, 53]
 test_bst = Baum(Node(numbers[0]))
 for value in numbers[1:]:
     test_bst.insert(value)
+
 print(test_bst)
 
 print("-------------")
@@ -37,7 +36,9 @@ for entry in DATASET:
     all_avl_bsts.append(avl_bst)
 
 print("fertig...\n-----------")
+
 print("Durchschnittliche Höhe der BSTs:")
 print(sum([tree.get_height() for tree in all_bsts]) / len(all_bsts))
+
 print("Vergleich: Durchschnittliche Höhe der AVL BSTs:")
 print(sum([tree.get_height() for tree in all_avl_bsts]) / len(all_avl_bsts))
